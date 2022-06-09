4345883cbc68402a298f9524fbd0c95067825fc4
ROD <104014121+prorodrigoh@users.noreply.github.com>
2022-06-09 16:51:15 -0400

    changed .env to secret to access DB

---
+++ dist/secrets.js
@@ -0,0 +1,3 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.default = "mongodb+srv://admin:admin123@cluster0.osjhu.mongodb.net/?retryWrites=true&w=majority";
--- src/gateway/mongo.ts
+++ src/gateway/mongo.ts
@@ -1,12 +1,13 @@
 import { MongoClient } from "mongodb";
+import MONGO_URL from "../secrets";
 
 export const getDb = async () => {
-  const client = new MongoClient(process.env.MONGO_URL!);
+  const client = new MongoClient(MONGO_URL!);
   await client.connect();
   return client.db("diet-app");
 };
 
 export const closeDb = async () => {
-  const client = new MongoClient(process.env.MONGO_URL!);
+  const client = new MongoClient(MONGO_URL!);
   await client.close();
 };
