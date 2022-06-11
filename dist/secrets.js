3d5f9a7b5c25e2e668c5a038c1312270963ec1fe
Rodrigo Henriques <104014121+prorodrigoh@users.noreply.github.com>
2022-06-11 15:11:52 -0400

    Deleted dist directory
    TypeS script build will recreated it

--- dist/secrets.js
+++
@@ -1,3 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = "mongodb+srv://admin:admin123@cluster0.osjhu.mongodb.net/?retryWrites=true&w=majority";
